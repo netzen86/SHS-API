492e20575fb51aad9a60b2b71e7adef3821e6c4b
netzen86 <netzen0su@gmail.com>
2023-11-27 00:32:31 +0500

    rem creation registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,19 +24,26 @@ resource "yandex_vpc_subnet" "nz-net" {
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
-
 # resource "yandex_container_registry" "nz-registry" {
 #   name = "nz-registry"
 # }
 
 locals {
   folder_id = "b1g5tv4fsuuk2l9gvd1p"
+  registry_id = "crpbccj0cfhnv6t6ocnd"
   service-accounts = toset([
-    "catgpt-container",
+    # "shs-container",
+    "shs-ig-sa"
   ])
-  catgpt-container-roles = toset([
-    "container-registry.images.puller",
-    "monitoring.editor",
+  # shs-container-roles = toset([
+  #   "container-registry.images.puller",
+  #   "monitoring.editor",
+  # ])
+  shs-ig-sa-roles = toset([
+    "compute.editor",
+    "iam.serviceAccounts.user",
+    "vpc.publicAdmin",
+    "vpc.user",
   ])
 }
 
@@ -44,105 +51,76 @@ resource "yandex_iam_service_account" "service-accounts" {
   for_each = local.service-accounts
   name     = each.key
 }
-resource "yandex_resourcemanager_folder_iam_member" "catgpt-container" {
-  for_each  = local.catgpt-container-roles
+
+# resource "yandex_resourcemanager_folder_iam_member" "shs-container-roles" {
+#   for_each  = local.shs-container-roles
+#   folder_id = local.folder_id
+#   member    = "serviceAccount:${yandex_iam_service_account.service-accounts["shs-container"].id}"
+#   role      = each.key
+# }
+
+resource "yandex_resourcemanager_folder_iam_member" "shs-ig-sa-roles" {
+  for_each  = local.shs-ig-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-container"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["shs-ig-sa"].id}"
   role      = each.key
 }
 
 data "yandex_compute_image" "coi" {
   family = "container-optimized-image"
 }
-resource "yandex_compute_instance" "catgpt-1" {
-  platform_id        = "standard-v2"
-  service_account_id = yandex_iam_service_account.service-accounts["catgpt-container"].id
-  resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+
+resource "yandex_compute_instance_group" "shs" {
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.shs-ig-sa-roles
+  ]
+  name               = "shs"
+  service_account_id = yandex_iam_service_account.service-accounts["shs-ig-sa"].id
+  allocation_policy {
+    zones = ["ru-central1-a"]
   }
-  scheduling_policy {
-    preemptible = true
+  deploy_policy {
+    max_unavailable = 1
+    max_creating    = 2
+    max_expansion   = 3
+    max_deleting    = 2
   }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.nz-net.id
-    nat       = true
-  }
-  boot_disk {
-    initialize_params {
-      type     = "network-hdd"
-      size     = "30"
-      image_id = data.yandex_compute_image.coi.id
+  scale_policy {
+    fixed_scale {
+      size = 2
     }
   }
-  metadata = {
-    docker-compose = file("${path.module}/docker-compose.yaml")
-    user-data = "${file("${path.module}/cloud_config.yaml")}"
-  }
-}
-
-resource "yandex_compute_instance" "catgpt-2" {
-  platform_id        = "standard-v2"
-  service_account_id = yandex_iam_service_account.service-accounts["catgpt-container"].id
-  resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
-  }
-  scheduling_policy {
-    preemptible = true
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.nz-net.id
-    nat       = true
-  }
-  boot_disk {
-    initialize_params {
-      type     = "network-hdd"
-      size     = "30"
-      image_id = data.yandex_compute_image.coi.id
+  instance_template {
+    platform_id        = "standard-v2"
+    service_account_id = yandex_iam_service_account.service-accounts["shs-ig-sa"].id
+    resources {
+      cores         = 2
+      memory        = 1
+      core_fraction = 5
     }
-  }
-  metadata = {
-    docker-compose = file("${path.module}/docker-compose.yaml")
-    user-data = "${file("${path.module}/cloud_config.yaml")}"
-  }
-}
-
-resource "yandex_lb_target_group" "nz-tg" {
-  name      = "nz-tg"
-  region_id = "ru-central1"
-
-  target {
-    subnet_id = "${yandex_vpc_subnet.nz-net.id}"
-    address   = "${yandex_compute_instance.catgpt-1.network_interface.0.ip_address}"
-  }
-  target {
-    subnet_id = "${yandex_vpc_subnet.nz-net.id}"
-    address   = "${yandex_compute_instance.catgpt-2.network_interface.0.ip_address}"
-  }
-}
-
-
-resource "yandex_lb_network_load_balancer" "nz-lb" {
-  name = "nz-lb"
-  deletion_protection = "false"
-  listener {
-    name = "nz-list"
-    port = 8080
-    external_address_spec {
-      ip_version = "ipv4"
+    scheduling_policy {
+      preemptible = true
     }
-  }
-  attached_target_group {
-    target_group_id = "${yandex_lb_target_group.nz-tg.id}"
-    healthcheck {
-      name = "nz-tg"
-      http_options {
-        port = 8080
-        path = "/ping"
+    network_interface {
+      # network_id = yandex_vpc_network.nz-net.id
+      subnet_ids = ["${yandex_vpc_subnet.nz-net.id}"]
+      nat        = true
+    }
+    boot_disk {
+      initialize_params {
+        type     = "network-hdd"
+        size     = "30"
+        image_id = data.yandex_compute_image.coi.id
       }
+    }
+    metadata = {
+      docker-compose = templatefile(
+        "${path.module}/docker-compose.yaml",
+        {
+          registry_id = local.registry_id,
+        }
+      )      
+      user-data      = "${file("${path.module}/cloud_config.yaml")}"
     }
   }
 }
